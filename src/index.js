3d5d66cdd9227978ac1559f6e244afff61c40d0c
Your Name <you@example.com>
2020-03-22 14:03:30 +0300

    feat: arrays task after fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return array.reduce((value, currentValue) => currentValue < value ? currentValue : value);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return array.reduce((value, currentValue) => currentValue > value ? currentValue : value);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+  return array.reduce( (value, currentValue) => value + currentValue) / array.length;
 }
